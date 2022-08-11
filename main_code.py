fbb26c237fc6700215efd94cf43b64b0342768e0
ElectableRuby31 <gilramot@outlook.com>
2022-08-11 14:39:39 +0300

    Co-authored-by: toto99ii <toto99ii@users.noreply.github.com>

--- main_code.py
+++ main_code.py
@@ -1,6 +1,16 @@
+from collections import OrderedDict
 import pywikibot
+import csv
+
 def main():
-    site = pywikibot.Site('he', 'wikipedia')
+    startarticle = input()
+    endarticle = input()
+    site = pywikibot.Site("he", "wikipedia")
+    page = pywikibot.Page(site, startarticle)
+    item = pywikibot.ItemPage.fromPage(page)
+    
+    print("works!")
+
 
 if __name__ == '__main__':
     main()
