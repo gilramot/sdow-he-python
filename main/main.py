ee0af0a9279f7d9ee0a0f18c6d79a05f01347d65
toto99ii <omertoto99ii@gmail.com>
2022-08-11 16:38:57 +0300

    fixed some issues

--- main/main.py
+++ main/main.py
@@ -2,38 +2,44 @@ import pywikibot
 import weblinkchecker
 startarticle = ''
 endarticle = ''
-def get_links(links):
+
+def merge(g1, g2):
+    for i in g1:
+        yield i
+    for i in g2:
+        yield i
+
+def get_links(gen_of_links):
     a = set()
-    for link in links:
+    for link in gen_of_links:
             a.update(link)
     return a
 
-def get_all_links(gen_of_links):
-    b = set()
-    for links in gen_of_links:
-        b.update(get_links(links))
-    return b
-
 def get_all_links_rec(gen_of_links, num):
-    s = set()
-    s = get_all_links(gen_of_links)
+    s = get_links(gen_of_links)
     if endarticle in s:
         print("finished " + num)
     else:
         num += 1
         new_s = set()
+        bool = True
         for article in s:
-            g = (weblinkchecker.weblinks_from_text(article))
+            if bool:
+                g = weblinkchecker.weblinks_from_text(article)
+                bool = False
+            else:
+                g0 = weblinkchecker.weblinks_from_text(article)
+                g = merge(g, g0)
         get_all_links_rec(g, num)
 
 def main():
     startarticle = input("enter start article: ")
     endarticle = input("Enter end article: ")
     print("")
-    links1 = weblinkchecker.weblinks_from_text(startarticle)
-    print(links1)
-    print(type(links1))
-    get_all_links_rec(links1, 1)
+    links_gen = weblinkchecker.weblinks_from_text(startarticle)
+    print(links_gen)
+    print(type(links_gen))
+    get_all_links_rec(links_gen, 1)
     
 if __name__ == '__main__':
     main()
