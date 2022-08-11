1397d161468eeea1a372fe1b8a8770291a3f1232
ElectableRuby31 <gilramot@outlook.com>
2022-08-11 15:26:32 +0300

    added a bunch of stuff

--- main/main.py
+++ main/main.py
@@ -1,7 +1,5 @@
-from collections import OrderedDict
 import pywikibot
-import csv
-import main.weblinkchecker as weblinkchecker
+import weblinkchecker
 
 def main():
     startarticle = input()
@@ -10,9 +8,14 @@ def main():
     page = pywikibot.Page(site, startarticle)
     item = pywikibot.ItemPage.fromPage(page)
     
-    weblinkchecker.weblinks_from_text(startarticle)
-    g-=gen()
+    links1 = weblinkchecker.weblinks_from_text(startarticle)
 
+    def get_links(links):
+        a = set()
+        for link in links:
+            a.update(link)
+    
+    s = get_links(links1)
 
 if __name__ == '__main__':
     main()
