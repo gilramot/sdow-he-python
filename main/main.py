d3b91d12cf026c967fa4d34b2f307d8ba1a4bab0
ElectableRuby31 <gilramot@outlook.com>
2022-08-11 15:50:16 +0300

    started working on english generator

--- main/main.py
+++ main/main.py
@@ -1,21 +1,39 @@
 import pywikibot
 import weblinkchecker
+startarticle = ''
+endarticle = ''
+def get_links(links):
+    a = set()
+    for link in links:
+            a.update(link)
+    return a
+
+def get_all_links(gen_of_links):
+    b = set()
+    for links in gen_of_links:
+        b.update(get_links(links))
+    return b
+
+def get_all_links_rec(gen_of_links, num):
+    s = set()
+    s = get_all_links(gen_of_links)
+    if endarticle in s:
+        print("finished " + num)
+    else:
+        num += 1
+        new_s = set()
+        for article in s:
+            g = (weblinkchecker.weblinks_from_text(article))
+        get_all_links_rec(g, num)
 
 def main():
-    startarticle = input()
-    endarticle = input()
-    site = pywikibot.Site("he", "wikipedia")
-    page = pywikibot.Page(site, startarticle)
-    item = pywikibot.ItemPage.fromPage(page)
-    
+    startarticle = input("enter start article: ")
+    endarticle = input("Enter end article: ")
+    print("")
     links1 = weblinkchecker.weblinks_from_text(startarticle)
-
-    def get_links(links):
-        a = set()
-        for link in links:
-            a.update(link)
+    print(links1)
+    print(type(links1))
+    get_all_links_rec(links1, 1)
     
-    s = get_links(links1)
-
 if __name__ == '__main__':
     main()
