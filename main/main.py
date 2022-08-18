90e8f642037b8c11fe938b7ec384e012707116e0
ElectableRuby31 <gilramot@outlook.com>
2022-08-18 21:03:11 +0300

    fixed generator function returning nothing

--- main/main.py
+++ main/main.py
@@ -7,31 +7,28 @@ wiki_wiki = wikipediaapi.Wikipedia('he')
 def get_all_links_rec(pages, num, endarticle):
     new_pages = []
     for page in pages:
-        page_py = wiki_wiki.page(page)
-        new_pages_from_page = get_links(page_py)
-        if find(new_pages_from_page, endarticle):
-            print("finished in " + num + " steps")
-            exit(0)
-        new_pages+=new_pages_from_page
+        wiki_page = wiki_wiki.page(page)
+        new_links_pages = get_links(wiki_page)
+        if endarticle == page:
+            return num
+        new_pages.extend(new_links_pages)
     get_all_links_rec(new_pages, num+1, endarticle)
     
 def get_links(page):
     lings = page.links
-    returnlinks = []
+    returnarticles = []
     forbidden_titles = ["שיחה:","משתמש:","שיחת משתמש:","ויקיפדיה:","שיחת ויקיפדיה:","קובץ:","שיחת קובץ:","מדיה ויקי:","שיחת מדיה ויקי:","תבנית:","שיחת תבנית:","עזרה:","שיחת עזרה:","קטגוריה:","שיחת קטגוריה:","פורטל:","שיחת פורטל:","ספר:","שיחת ספר:","טיוטה:","שיחת טיוטה:","TimedText:","TimedText talk:","יחידה:","שיחת יחידה:","גאדג'ט:","שיחת גאדג'ט:","הגדרת גאדג'ט:"]
     for i in range (len(lings)):
         res = list(filter(list(lings.keys())[i].startswith, forbidden_titles)) != []
         if not res:
-            returnlinks.append(list(lings.keys())[i])
-    return returnlinks
-            
+            returnarticles.append(list(lings.keys())[i])
+    return returnarticles
+    
     
 
 def main():
     startarticle = input("enter start article: ")
     endarticle = input("Enter end article: ")
-    page_py = wiki_wiki.page(startarticle)
-    get_all_links_rec([page_py], 0 ,endarticle)
-    
+    print(get_all_links_rec([startarticle], 0 ,endarticle))
 if __name__ == '__main__':
     main()
