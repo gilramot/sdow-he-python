e91b5202534c7a82d8bd6e86e81f6efd84a2524a
ElectableRuby31 <gilramot@outlook.com>
2022-08-18 19:45:33 +0300

    fixed generator, removed unnecesarry pages

--- main/main.py
+++ main/main.py
@@ -1,30 +1,36 @@
+from gettext import find
 import wikipediaapi
 startarticle = ''
 endarticle = ''
-wiki_wiki = wikipediaapi.Wikipedia('en')
+wiki_wiki = wikipediaapi.Wikipedia('he')
 
 def get_all_links_rec(pages, num, endarticle):
     new_pages = []
     for page in pages:
-        for i in page.links:
-            new_pages += wiki_wiki.page(i)
-        if endarticle == page:
+        page_py = wiki_wiki.page(page)
+        new_pages_from_page = get_links(page_py)
+        if find(new_pages_from_page, endarticle):
             print("finished in " + num + " steps")
+            exit(0)
+        new_pages+=new_pages_from_page
     get_all_links_rec(new_pages, num+1, endarticle)
     
 def get_links(page):
     lings = page.links
+    returnlinks = []
+    forbidden_titles = ["שיחה:","משתמש:","שיחת משתמש:","ויקיפדיה:","שיחת ויקיפדיה:","קובץ:","שיחת קובץ:","מדיה ויקי:","שיחת מדיה ויקי:","תבנית:","שיחת תבנית:","עזרה:","שיחת עזרה:","קטגוריה:","שיחת קטגוריה:","פורטל:","שיחת פורטל:","ספר:","שיחת ספר:","טיוטה:","שיחת טיוטה:","TimedText:","TimedText talk:","יחידה:","שיחת יחידה:","גאדג'ט:","שיחת גאדג'ט:","הגדרת גאדג'ט:"]
     for i in range (len(lings)):
-        print(list(lings.keys())[i])
-    
+        res = list(filter(list(lings.keys())[i].startswith, forbidden_titles)) != []
+        if not res:
+            returnlinks.append(list(lings.keys())[i])
+    return returnlinks
+            
     
 
 def main():
     startarticle = input("enter start article: ")
     endarticle = input("Enter end article: ")
     page_py = wiki_wiki.page(startarticle)
-    get_links(page_py)
-    exit(0)
     get_all_links_rec([page_py], 0 ,endarticle)
     
 if __name__ == '__main__':
